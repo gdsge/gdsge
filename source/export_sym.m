d91828cae702a38f93f5476c6e1e51cb32ca8e97
Wenlan Luo <luowenlan@gmail.com>
2022-08-29 18:12:30 +0800

    add reationale for exporting symbolic math

--- source/export_sym.m
+++ source/export_sym.m
@@ -1,3 +1,8 @@
-% This file is a part of GDSGE. License is Apache License, Version 2.0: http://github.com/gdsge/gdsge/LICENSE
+% Needs older version of the Symbolic Math toolbox which MATLAB 2017b+ does not
+% support. For license issues, cannot distribute MATLAB's library in m files.
+% The temporary solution is to compile the sym class into p files and distribute the p
+% files instead of the source codes, and list Symbolic Math toolbox as one of the dependencies.
+% Licensed MATLAB users can acquire a copy of the older version (<=2017b) of the Symbolic
+% Math toolbox and put it in the folder for explicit uses.
 pcode('@sym/private');
 pcode('@sym');
