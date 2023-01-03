d712aa35a24c84aeb766e5a05e1e0a6740ca3e34
dan.cao <vudan.cao@gmail.com>
2023-01-03 09:48:06 -0500

    update license files

--- source/gdsge.m
+++ source/gdsge.m
@@ -1,3 +1,27 @@
+% Copyright (C) 2020 GDSGE Dan Cao, Wenlan Luo and Guangyu Nie
+%
+% gdsge.cln2020@gmail.com 
+%
+% Licensed under the Apache License, Version 2.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at
+% 
+%     http://www.apache.org/licenses/LICENSE-2.0
+% 
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
+% 
+% The license allows you to use, compose and distribute the GDSGE compiler 
+% or generated codes freely. However, it is requested that the companion paper 
+% be cited:
+% 
+% **Cao, Dan and Luo, Wenlan and Nie, Guangyu, Global DSGE Models (April 1, 2020). 
+% Available at SSRN: https://ssrn.com/abstract=3569013 
+% or http://dx.doi.org/10.2139/ssrn.3569013**
+
 function eq = gdsge(modelName)
 % This file is a part of GDSGE. License is Apache License, Version 2.0: http://github.com/gdsge/gdsge/LICENSE
 
